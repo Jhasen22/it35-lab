fa67b0b8709f7fd13e42babfefa8958b835bd16f
jonard <jhasenambogna119@gmail.com>
2025-05-13 16:38:09 +0800

    Updates

--- assets/ios.transition-legacy-B-hRl909.js
+++ assets/ios.transition-legacy-B-hRl909.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-jY0YmE-m.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-y64XY9yn.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
